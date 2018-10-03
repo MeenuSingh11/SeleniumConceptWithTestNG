66716364805cdd4e4965abc6ed79519e1f7df8eb
Meenu Singh <bitmeenusingh.11@gmail.com>
2018-10-03 22:49:22 +0400

    modified the priorityConceptTestNG file

--- src/test/java/com/TestNG/Practise/PriorityConceptTestNG.java
+++ src/test/java/com/TestNG/Practise/PriorityConceptTestNG.java
@@ -1,35 +1,54 @@
 package com.TestNG.Practise;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class PriorityConceptTestNG {
 
+	WebDriver driver;
+
 	@BeforeMethod
 	public void setup() {
-		System.out.println("Introduction");
+		System.setProperty("webdriver.chrome.driver",
+				"C:\\Automation\\Softwares\\chromedriver_win32\\chromedriver.exe");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().pageLoadTimeout(40, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		driver.get("http://www.google.com");
 
 	}
 
 	@Test(priority = 1)
-	public void introduction() {
-		System.out.println("My name is Meenu Singh");
+	public void googleTitleTest() {
+		String title = driver.getTitle();
+		System.out.println(title);
+		Assert.assertEquals("Google", title);
 	}
 
 	@Test(priority = 3)
-	public void work() {
-		System.out.println("I am working here in Emirates Airline");
-
+	public void googleLogoTest() {
+		boolean b = driver.findElement(By.xpath("//*[@id=\"hplogo\"]")).isDisplayed();
+		Assert.assertEquals(true, b);
 	}
 
 	@Test(priority = 2)
-	public void birthOfPlace() {
-		System.out.println("I born/broughtup in India");
+	public void mailLinkTest() {
+		boolean b = driver.findElement(By.linkText("Gmail")).isDisplayed();
+		Assert.assertEquals(true, b);
 	}
 
 	@AfterMethod
 	public void tearDown() {
-		System.out.println("End");
+		driver.quit();
 	}
+
 }
